ad556b7c5af253b2e7c2b4ae9274d66cd766fe5f
goledenpa <pablo.dorado.garcia.2002@gmail.com>
2026-03-22 02:16:58 +0100

    Refactor crawler logic; add SetTimeRange abstraction
    Refactor Crawler class for improved modularity and readability by extracting per-player crawling, match importing, and logging into dedicated methods. Introduce constants for delays and time calculations. Add SetTimeRange record to encapsulate patch/set time ranges, replacing direct date manipulation. Enhance exception handling and logging. These changes improve maintainability and code structure.

---
+++ TFTStats.Core/Entities/SetTimeRange.cs
@@ -0,0 +1,21 @@
+﻿namespace TFTStats.Core.Entities
+{
+    public record SetTimeRange
+    {
+
+        public long PatchStartTime { get; set; }
+        public long PatchEndTime { get; set; }
+
+        public SetTimeRange(TFTPatch patch)
+        {
+            PatchStartTime = ((DateTimeOffset)patch.StartDate).ToUnixTimeSeconds();
+            PatchEndTime = ((DateTimeOffset)patch.EndDate!).ToUnixTimeSeconds();
+        }
+
+        public SetTimeRange(TFTPatch latestPatch, TFTPatch earliestPatch)
+        {
+            PatchStartTime = ((DateTimeOffset)earliestPatch.StartDate).ToUnixTimeSeconds();
+            PatchEndTime = ((DateTimeOffset)latestPatch.EndDate!).ToUnixTimeSeconds();
+        }
+    }
+}
--- TFTStats.Presentation/Crawler.cs
+++ TFTStats.Presentation/Crawler.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using TFTStats.Core.Entities;
 using TFTStats.Core.Infrastructure.Importers.Interfaces;
 using TFTStats.Core.Mappings;
 using TFTStats.Core.Repositories.Interfaces;
@@ -8,6 +9,10 @@ namespace TFTStats.Presentation
 {
     public class Crawler
     {
+        private const int crawlerCheckDelayMs = 10000;
+        private const int errorRetryDelayMs = 5000;
+        private const int hourInSeconds = 3600;
+
         private readonly RiotTFTMatchService _matchService;
         private readonly IMatchRepository _matchRepo;
         private readonly IRiotDataImporter _importer;
@@ -25,12 +30,7 @@ namespace TFTStats.Presentation
 
         public async Task RunAsync(string cluster, int targetSet, CancellationToken ct)
         {
-            DateTime longTime = (await _patchRepository.GetLastPatch(targetSet)).StartDate;
-            DateTime startSetTime = (await _patchRepository.GetFirstPatch(targetSet)).StartDate;
-            long longTimeStart = ((DateTimeOffset)longTime).ToUnixTimeSeconds();
-            long longStartSetTime = ((DateTimeOffset)startSetTime).ToUnixTimeSeconds();
-            long currentQueryStartTime;
-
+            var setTimeRange = await GetSetTimeRangeAsync(targetSet);
             _logger.LogInformation("[Crawler] Starting crawl on cluster: {cluster}", cluster);
 
             while (!ct.IsCancellationRequested)
@@ -42,98 +42,107 @@ namespace TFTStats.Presentation
                     if (string.IsNullOrEmpty(puuid))
                     {
                         _logger.LogInformation("[Crawler] No peding players in DB. Waiting 10 seconds...");
-                        await Task.Delay(10000, ct);
+                        await Task.Delay(crawlerCheckDelayMs, ct);
                         continue;
                     }
 
-                    if (lastCrawledAt.HasValue)
-                    {
-                        currentQueryStartTime = new DateTimeOffset(lastCrawledAt.Value).ToUnixTimeSeconds() - 3600;
-                    }
-                    else
-                    {
-                        currentQueryStartTime = longTimeStart;
-                    }
-
-                    var startTimeAsOffset = DateTimeOffset.FromUnixTimeSeconds(currentQueryStartTime);
-                    var localStart = startTimeAsOffset.ToLocalTime();
-                    var timeSpan = DateTimeOffset.UtcNow - startTimeAsOffset;
-
-                    string searchRangeInfo;
-
-                    if(currentQueryStartTime == longStartSetTime)
-                    {
-                        searchRangeInfo = $"Start of Set ({localStart:MMM dd, yyyy HH:mm})";
-                    }
-                    else
-                    {
-                        string timeAgo = timeSpan.TotalDays >= 1
-                            ? $"{(int)timeSpan.TotalDays} days"
-                            : $"{(int)timeSpan.TotalHours} hours";
-
-                        searchRangeInfo = $"{localStart:MMM dd yyyy, yyyy HH:mm} ({timeAgo} ago)";
-                    }
-
-                    _logger.LogInformation("[Crawler] Target Found: {puuid}", puuid);
-                    _logger.LogInformation("[Crawler] Searching from: {searchRangeInfo}", searchRangeInfo);
-
-                    var allIds = await _matchService.GetSetMatchIdsAsync(cluster, puuid, currentQueryStartTime, ct);
-
-                    var uniqueIds = allIds.Distinct().ToList();
-
-                    if(uniqueIds.Count != allIds.Count)
-                    {
-                        _logger.LogInformation("[System] Duplicated ID in Riot GetMatchs");
-                    }
-
-                    var newIds = await _matchRepo.FilterNewMatchIdsAsync(uniqueIds);
-
-                    if (newIds.Count > 0)
-                    {
-                        // Calculation for clarity
-                        int existingCount = uniqueIds.Count - newIds.Count;
-
-                        _logger.LogInformation("[Crawler] Found {newIdsCount} new matches ({existingCount} already in DB, {uniqueIdsCount} total for this set).", newIds.Count, existingCount, uniqueIds.Count);
-                        _logger.LogInformation("[Crawler] Starting Ingestion...");
-
-                        var dtoStream = _matchService.GetMatchStreamAsync(cluster, newIds, ct);
-                        var entityStream = dtoStream
-                            .Where(x => x.Info.TftSetNumber == targetSet)
-                            .Select(x => x.ToEntity())
-                            .Where(x => x.Participants.Count != 0);
-
-                        await _importer.ImportMatchStremAsync(entityStream, ct);
-                    }
-                    else
-                    {
-                        _logger.LogInformation("[Crawler] 0 new matches found. (Player is up to date with {uniqueIdsCount} matches).", uniqueIds.Count);
-                    }
-
-                    await _matchRepo.MarkPlayerAsCrawledAsync(puuid);
-
-                    int totalPlayers = await _matchRepo.GetTotalPlayerCountAsync();
-                    _logger.LogInformation("[Crawler] Target {puuid} finished. Total Players Known: {totalPlayers}", puuid, totalPlayers);
+                    await CrawlPlayerAsync(cluster, targetSet, puuid, lastCrawledAt, setTimeRange, ct);
+                }
+                catch (OperationCanceledException) when (ct.IsCancellationRequested)
+                {
+                    _logger.LogInformation("[Crawler] Shutdown requested by user. Stopping...");
+                    break;
                 }
                 catch (OperationCanceledException ex)
                 {
-                    if (ct.IsCancellationRequested)
-                    {
-                        _logger.LogInformation("[Crawler] Shutdown requested by user. Stopping...");
-                        break;
-                    }
-                    else
-                    {
-                        _logger.LogError("[Crawler ERROR] A task timed out (Internal Cancellation): {exMessage}", ex.Message);
-                        await Task.Delay(5000, ct);
-                    }
+                    _logger.LogError("[Crawler ERROR] A task timed out (Internal Cancellation): {exMessage}", ex.Message);
+                    await Task.Delay(errorRetryDelayMs, ct);
                 }
                 catch (Exception ex)
                 {
                     _logger.LogError("[Crawler ERROR] {exMessage}", ex.Message);
-
-                    await Task.Delay(5000, ct);
+                    await Task.Delay(errorRetryDelayMs, ct);
                 }
             }
+        }
+
+        private async Task<SetTimeRange> GetSetTimeRangeAsync(int targetSet)
+        {
+            var lastPatch = await _patchRepository.GetLastPatch(targetSet);
+            //var firstPatch = await _patchRepository.GetFirstPatch(targetSet);
+            
+            return new SetTimeRange(lastPatch);
+        }
+
+        private async Task CrawlPlayerAsync(string cluster, int targetSet, string puuid, DateTime? lastCrawledAt, SetTimeRange setTimeRange, CancellationToken ct)
+        {
+            long queryStartTime = lastCrawledAt.HasValue
+                ? new DateTimeOffset(lastCrawledAt.Value).ToUnixTimeSeconds() - hourInSeconds
+                : setTimeRange.PatchStartTime;
+
+            _logger.LogInformation("[Crawler] Target Found: {puuid}", puuid);
+
+            LogSearchRange(queryStartTime, setTimeRange.PatchEndTime);
+
+            var allIds = await _matchService.GetSetMatchIdsAsync(cluster, puuid, queryStartTime, ct);
+            var uniqueIds = allIds.Distinct().ToList();
+
+            if (uniqueIds.Count != allIds.Count)
+            {
+                _logger.LogInformation("[System] Duplicated ID in Riot GetMatchs");
+            }
+
+            var newIds = await _matchRepo.FilterNewMatchIdsAsync(uniqueIds);
+
+            if (newIds.Count > 0)
+            {
+                await ImportNewMatchesAsync(cluster, targetSet, newIds, uniqueIds, ct);
+            }
+            else
+            {
+                _logger.LogInformation("[Crawler] 0 new matches found. (Player is up to date with {uniqueIdsCount} matches).", uniqueIds.Count);
+            }
+
+            await _matchRepo.MarkPlayerAsCrawledAsync(puuid);
+
+            int totalPlayers = await _matchRepo.GetTotalPlayerCountAsync();
+            _logger.LogInformation("[Crawler] Target {puuid} finished. Total Players Known: {totalPlayers}", puuid, totalPlayers);
+        }
+
+        private void LogSearchRange(long queryStartTime, long setEndTime)
+        {
+            var startTimeAsOffset = DateTimeOffset.FromUnixTimeSeconds(queryStartTime);
+            var localStart = startTimeAsOffset.ToLocalTime();
+            var timeSpan = DateTimeOffset.UtcNow - startTimeAsOffset;
+
+            string searchRangeInfo = queryStartTime == setEndTime
+                ? $"Start of Set ({localStart:MMM dd, yyyy HH:mm})"
+                : $"{localStart:MMM dd, yyyy HH:mm} ({GetTimeAgoString(timeSpan)} ago)";
+
+            _logger.LogInformation("[Crawler] Searching from: {searchRangeInfo}", searchRangeInfo);
+        }
+
+        private string GetTimeAgoString(TimeSpan timeSpan)
+        {
+            return timeSpan.TotalDays >= 1
+                ? $"{(int)timeSpan.TotalDays} days"
+                : $"{(int)timeSpan.TotalHours} hours";
+        }
+
+        private async Task ImportNewMatchesAsync(string cluster, int targetSet, List<string> newIds, List<string> uniqueIds, CancellationToken ct)
+        {
+            int existingCount = uniqueIds.Count - newIds.Count;
+
+            _logger.LogInformation("[Crawler] Found {newIdsCount} new matches ({existingCount} already in DB, {uniqueIdsCount} total for this patch).", newIds.Count, existingCount, uniqueIds.Count);
+            _logger.LogInformation("[Crawler] Starting Ingestion...");
+
+            var dtoStream = _matchService.GetMatchStreamAsync(cluster, newIds, ct);
+            var entityStream = dtoStream
+                .Where(x => x.Info.TftSetNumber == targetSet)
+                .Select(x => x.ToEntity())
+                .Where(x => x.Participants.Count != 0);
+
+            await _importer.ImportMatchStremAsync(entityStream, ct);
         }
     }
 }
